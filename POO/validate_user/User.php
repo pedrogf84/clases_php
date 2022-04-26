9623e167dc69e70b09089b03a91854847949b799
pedrogf84 <pedrogonzalezfernandez@gmail.com>
2022-04-26 17:59:15 +0200

    ejercicios ficheros y clases

--- POO/validate_user/User.php
+++ POO/validate_user/User.php
@@ -42,19 +42,73 @@ class User
         return "USER:<br />name : $this->name<br />password: $this->password";
     }
 
-    function validatePassword($user)
+    /*
+    * function validatePassword
+    * 
+    * params: $user : User
+    *
+    * includes: functions containsSpecialChar, checkStrength
+    *
+    * return: string
+    */
+
+    function validatePassword($user) : string
     {
         if (count_chars($user->password) >= 8 && $this->containsSpecialChar($user->password))
-            return "Password válido";
+            return "Password válido, la fuerza del password es: ".$this->checkStrength($user->password);
         else return "Password no válido, comprueba que tienes al menos: <br />- 8 caractéres<br />- alguno de estos caracteres especiales: '.' ',' '-' '_' ';'";
     }
 
-    function containsSpecialChar($string): bool
+
+    /*
+    * function containsSpecialChar
+    * 
+    * params: $password
+    *
+    * return: boolean
+    */
+    function containsSpecialChar($password): bool
     {
         $specialChars = array('.', ',', '-', '_', ';');
         foreach ($specialChars as $item) {
-            if (str_contains($string, $item)) return true;
+            if (str_contains($password, $item)) return true;
         }
         return false;
+    }
+
+    /*
+    * function checkStrength
+    * 
+    * params: $password
+    *
+    * return: password strength form very weak to very strong.
+    */
+
+    function checkStrength($password) : string
+    {
+        $number = preg_match('@[0-9]@', $password);
+        $uppercase = preg_match('@[A-Z]@', $password);
+        $lowercase = preg_match('@[a-z]@', $password);
+        $specialChars = preg_match('@[^\w]@', $password);
+
+        $strength = array($number, $uppercase, $lowercase, $specialChars);
+
+        switch (count(array_filter($strength))) {
+            case 1:
+                return "muy débil";
+                break;
+            case 2:
+                return "débil";
+                break;
+            case 3:
+                return "fuerte";
+                break;
+            case 4:
+                return "muy fuerte";
+                break;
+            default:
+                return "not evaluable";
+                break;
+        }
     }
 }
